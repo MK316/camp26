1f39df9163cc08575ac90a5a2dc82a33e77555a5
MK <99416359+MK316@users.noreply.github.com>
2026-01-20 17:40:38 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -6,13 +6,13 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-st.title("Survey Dashboard (Q01–Q12)")
+st.markdown("#### ❄️ Survey Dashboard (Q01–Q12): 참여 연구자 공유용")
 st.write(
     """
-이 대시보드는 설문 데이터(Q01–Q12, 1–6 리커트)를 요약하고,
+이 대시보드는 1/20일에 마친 설문 데이터(Q01–Q12, 1–6 리커트)를 요약하고,
 필드/학년/전공 분야에 따라 결과를 수치와 그래프로 확인할 수 있도록 구성되었습니다.
 
-왼쪽 사이드바에서 페이지를 이동하세요.
+왼쪽 사이드바에서 메뉴를 선택하여 페이지를 이동하세요.
 """
 )
 
